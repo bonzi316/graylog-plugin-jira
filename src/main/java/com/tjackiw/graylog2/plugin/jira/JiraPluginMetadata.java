75da86dc6f56a2aaefd91464e8395711e4bf0ffa
bonzy316 bonzy316 <etienne.lebel@iaah.local>
2016-03-21 15:45:09 -0400

    Change required version of graylog to 1.3.3 (Required by getRawMessage())

--- src/main/java/com/tjackiw/graylog2/plugin/jira/JiraPluginMetadata.java
+++ src/main/java/com/tjackiw/graylog2/plugin/jira/JiraPluginMetadata.java
@@ -57,7 +57,7 @@ public class JiraPluginMetadata implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 0, 0);
+        return new Version(1, 0, 1);
     }
 
     @Override
@@ -67,7 +67,7 @@ public class JiraPluginMetadata implements PluginMetaData {
 
     @Override
     public Version getRequiredVersion() {
-        return new Version(1, 0, 0);
+        return new Version(1, 3, 3);
     }
 
     @Override
